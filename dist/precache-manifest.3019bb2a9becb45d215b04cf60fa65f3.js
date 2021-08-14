97aab8d40f23355e0db8a73637bf5cb0276446b5
drnareshchauhan <drnareshchauhan@gmail.com>
2021-08-14 18:12:37 +0530

    new kg

---
+++ dist/precache-manifest.3019bb2a9becb45d215b04cf60fa65f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b22d5129210a75fb7e6a54e9cae611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f6147c5383a3cba89b3",
+    "url": "/static/css/main.46b94f10.chunk.css"
+  },
+  {
+    "revision": "2f33b91159c74b7315dd",
+    "url": "/static/js/2.cd58c1e8.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.cd58c1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6147c5383a3cba89b3",
+    "url": "/static/js/main.117202c3.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
